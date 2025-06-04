9d0dbd15878658a3f00c8530ddb268e46afa6833
Wectornanime <wectornanime@gmail.com>
2025-06-03 22:47:49 -0300

    pre treino

--- utils/utils.py
+++ utils/utils.py
@@ -8,19 +8,19 @@ Functions:
 def get_sentiment(rating):
     """
     Retorna o sentimento correspondente ao rating, onde:
-    1 e 2 retorna 'Negativo',
-    3 retorna 'Neutro',
-    4 e 5 retorna 'Positivo'
+    1 e 2 retorna 'negative',
+    3 retorna 'neutral',
+    4 e 5 retorna 'positive'
 
     Args:
         rating (int): Avaliação numérica entre 1 e 5.
 
     Returns:
-        str: Sentimento associado à avaliação ('Positivo', 'Neutro' ou 'Negativo').
+        str: Sentimento associado à avaliação ('positive', 'neutral' ou 'negative').
     """
     if rating >= 4:
-        return 'Positivo'
+        return 'positive'
     elif rating <= 2:
-        return 'Negativo'
+        return 'negative'
     else: # rating == 3
-        return 'Neutro'
+        return 'neutral'
